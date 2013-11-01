b96fa0ec9c8649f13e6cc93f9eb00e0bf8b375ff
Stephen Moore <smoore.ms9@gmail.com>
2013-11-01 17:21:58 -0400

    Project Setup

---
+++ Source Code/Tailored Travels/src/com/twix/tailoredtravels/Authenticator.java
@@ -0,0 +1,13 @@
+package com.twix.tailoredtravels;
+
+public class Authenticator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Source Code/Tailored Travels/src/com/twix/tailoredtravels/DatabaseManager.java
@@ -0,0 +1,13 @@
+package com.twix.tailoredtravels;
+
+public class DatabaseManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
