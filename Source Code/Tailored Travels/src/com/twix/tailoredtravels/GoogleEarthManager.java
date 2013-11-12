347823ddb9139d06b2270e898c67da4d4ec6c6e7
Jtav1 <justintavares@gmail.com>
2013-11-12 05:43:49 -0500

    Added code to change path to kml file
    Changed GoogleEarthPath to accept a list of waypoints. Added KML
    generation code to GoogleEarthManager.

--- Source Code/Tailored Travels/src/com/twix/tailoredtravels/GoogleEarthManager.java
+++ Source Code/Tailored Travels/src/com/twix/tailoredtravels/GoogleEarthManager.java
@@ -1,5 +1,9 @@
 package com.twix.tailoredtravels;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 
 /**
@@ -14,17 +18,36 @@ public class GoogleEarthManager {
 	public GoogleEarthManager()
 	{
 		
+		
+		
 	}
 	
 	/**
 	 * Transforms a given path into a series of connected KML points.
 	 * 
-	 * @param _path The created path of Waypoints
+	 * @param _path The created path of Waypoints in LinkedList<Waypoint> format
 	 * @return the generated KML
 	 */
-	public Kml Path2KML(GoogleEarthPath _path)
+	public void Path2KML(GoogleEarthPath _path)
 	{
-		// TODO: This...
-		return null;
+		Kml output = new Kml();
+		
+		LinkedList<Waypoint> wPath = _path.getPath();
+		for(Waypoint w : wPath)
+		{
+			output.createAndSetPlacemark().withName(w.getName())
+			.withOpen(true).createAndSetPoint()
+			.addToCoordinates(w.getLatitude(), w.getLongitude());
+		}
+		
+		try {
+			output.marshal(new File("TEST_KML_OUTPUT.kml"));
+		} catch (FileNotFoundException e) {
+			// output.marshal creates the file TEST_KML_OUTPUT.kml.
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 }
