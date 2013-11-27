f3ee12a111dfdf7257f829ec4fba00102b730694
bama4 <bama4@umbc.edu>
2013-11-27 14:31:39 -0500

    Corrections to DistCalcDriver

--- Source Code/Tailored Travels/src/com/twix/tailoredtravels/DistCalcDriver.java
+++ Source Code/Tailored Travels/src/com/twix/tailoredtravels/DistCalcDriver.java
@@ -1,10 +1,11 @@
 package com.twix.tailoredtravels;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Handle calculation of shortest distance between given
- * start and end points. 
+ * start and end Waypoints. 
  * 
  * @author Mariama Barr-Dallas & Michael Tang
  * @version 2.0
@@ -12,63 +13,129 @@ import java.util.ArrayList;
 
 public class DistCalcDriver {
 
-	//Must know the arraylist <type> that will be recieved from
-	//Google Earth Data.
-	
-	/**@author Herman Kontcho
+	/**Calculates the distance of the given path
 	 * 
-	 * dist calculates the distance of the path
-	 *@param ArrayList<Waypoint> list
-	 *@return distance 
+	 * @param List<Waypoint> ap
+	 * @return
 	 */
-	public static double dist(ArrayList<Waypoint> list){
+	public static double dist(List<Waypoint> ap){
 		double distance = 0;
-		Waypoint  start = list.remove(0);
-		
-		while(!list.isEmpty()){
-			distance += start.distance(list.get(0));
-			if (list.isEmpty() == false){
-				start = list.remove(0);
+		Waypoint  start = ap.get(0);
+		int count = 1;
+		while(count < ap.size()){
+			distance += start.distance(ap.get(count));
+			if (ap.isEmpty() == false){
+				start = ap.get(count++);
 			}
 		}
 		
 		return distance;
 	}
 	
-	/**@author Herman Kontcho
+	/**@author - Herman Kontcho
+	 * Generates all the permutations of paths
 	 * 
-	 * shortDistAlgorithm calculates the shortest route
-	 * based on the given list and the start and end points
-	 * returns an arraylist of coordinates sorted for shortest distance
-	 * list should come with the start point as the first index, and the last index as the end point
+	 * @param original
+	 * @return
+	 */
+	public static List<List<Waypoint>> generatePermP(ArrayList<Waypoint> original) {
+		
+	     if (original.size() == 0) { 
+	    	 List<List<Waypoint>> result = new ArrayList<List<Waypoint>>();
+	    	 result.add(new ArrayList<Waypoint>());
+	    	 return result;
+	     	}
+	     Waypoint firstElement = original.remove(0);
+	     List<List<Waypoint>> returnValue = new ArrayList<List<Waypoint>>();
+	     List<List<Waypoint>> permutations = generatePermP(original);
+	     
+	     
+	     for (List<Waypoint> smallerPermutated : permutations) {
+	       for (int index=0; index <= smallerPermutated.size(); index++) {
+	    	   ArrayList<Waypoint> temp = new ArrayList<Waypoint>(smallerPermutated);
+	    	   temp.add(index, firstElement);
+	    	   returnValue.add(temp);
+	       }
+	     }
+	     return returnValue;
+	   }
+	
+	/**
+	 * @author - Herman Kontcho
+	 * Calculates the shortest path
 	 * 
-	 * @param list	list of parks that the user wants to go to
-	 * @return ArrayList<Waypoint> sorted arraylist of all waypoints
+	 * @param list
+	 * @return
 	 */
 	public static ArrayList<Waypoint> shortDistAlgorithm(ArrayList<Waypoint> list){
 		if (list == null || list.size() == 0){
 			return null;
 		}
 		
-		//Remove the starting point from the list and get the ending point
+		//remove the start and ending Waypoint
+		Waypoint start = list.remove(0);
+		Waypoint end = list.remove(list.size() - 1);
+		
+		//get all possible permutation of the remaining list
+		List<List<Waypoint>> temp = generatePermP(list);
+		
+		//get the first one
+		List<Waypoint> path1 = temp.get(0);
+		path1.add(end);
+		path1.add(0, start);
+		
+		//get the current distance
+		double curr_dis = dist(path1);
+		
+		for (int i = 1; i < temp.size(); i++){
+			
+			List<Waypoint> curr = temp.get(i);
+			curr.add(end);
+			curr.add(0, start);
+			
+			double d = dist(curr);
+			
+			if (curr_dis > d){
+				path1 = curr;
+				curr_dis = d;
+			}
+		}
+		ArrayList<Waypoint> lis = new ArrayList<Waypoint>();
+		
+		for(int i = 0; i < path1.size(); i++)
+			lis.add(path1.get(i));
+		return lis;
+	}
+	
+	/**
+	 * @author - Herman Kontcho
+	 * @param list
+	 * @return
+	 */
+	public static ArrayList<Waypoint> shortestPath(ArrayList<Waypoint> list){
+		if (list == null || list.size() == 0){
+			return null;
+		}
+		
+		//Remove the starting Waypoint from the list and get the ending Waypoint
 		Waypoint start = list.get(0);
 		Waypoint end = list.get(list.size() - 1);
 		
 		
-		//Set the curr point to be the starting point
+		//Set the curr Waypoint to be the starting Waypoint
 		Waypoint curr = list.remove(0);
 		
 		//create a temp list that will be holding the path along the iteration
 		ArrayList<Waypoint> path = new ArrayList<Waypoint>();
 		
-		//adding the starting point in the path
+		//adding the starting Waypoint in the path
 		path.add(start);
 		
 		//curretnce distance
 		double curr_dist = 0;
 		
 		
-		//loop till the curr point is the ending point
+		//loop till the curr Waypoint is the ending Waypoint
 		while (!curr.equals(end)){
 			double curr_temp_dist = curr_dist + curr.distance(list.get(0));
 			int closest_index = 0;
@@ -83,9 +150,9 @@ public class DistCalcDriver {
 				}
 			}
 			
-			//At this point closest_index will be the index of the closest point from 
+			//At this Waypoint closest_index will be the index of the closest Waypoint from 
 			//curr so we need to remove it from the list and update the distance 
-			//along with the curr_point
+			//along with the curr_Waypoint
 			curr_dist += curr.distance(list.get(closest_index));
 			curr = list.remove(closest_index);
 			path.add(curr);
@@ -93,7 +160,10 @@ public class DistCalcDriver {
 		return path;	
 	}
 	
-	
+	/**
+	 * Testing algorithm
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 
@@ -117,40 +187,7 @@ public class DistCalcDriver {
 		System.out.println("And the distance is " + dist(path));
 	
 	}
-	/**
-	public static ArrayList<Waypoint> shortDistAlgorithm(ArrayList<Waypoint> list, Waypoint destA, Waypoint destG)
-	{
-		
-		ArrayList<Waypoint> path = new ArrayList<Waypoint>();
-		Waypoint current = destA;
-		Waypoint next = null;
-		double smallest = Double.MAX_VALUE;
-		double distance;
-		path.add(current);
-		
-		for(int i=0; i<(list.size()-2); i++)
-		{
-			for(int j=0; j<list.size(); j++)
-			{
-				//determines the next nearest destination not searching previous targets or the end value 
-				if(!path.contains(list.get(j)) && list.get(j) != destG)
-				{
-					distance = current.distance(list.get(j));
-					if(smallest > distance)
-					{
-						smallest = distance;
-						next = list.get(j);
-					}
-				}
-			}
-			current = next;
-			path.add(current);
-			smallest = Double.MAX_VALUE;
-		}
-		path.add(destG);		
-		return path;
-	}
-	**/
+	
 	/**
 	 * totalDistance calculates the total distance covered by this path
 	 * 
