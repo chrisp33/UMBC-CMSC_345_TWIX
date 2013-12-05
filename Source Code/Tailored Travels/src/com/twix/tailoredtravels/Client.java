035e9b88d1807e57bf601e4ad9a72fc7576bfd57
Jtav1 <justintavares@gmail.com>
2013-12-04 21:33:44 -0500

    Re-did earlier changes
    Were overwritten

--- Source Code/Tailored Travels/src/com/twix/tailoredtravels/Client.java
+++ Source Code/Tailored Travels/src/com/twix/tailoredtravels/Client.java
@@ -11,12 +11,9 @@ package com.twix.tailoredtravels;
 import java.awt.Color;
 import java.awt.Desktop;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -31,23 +28,30 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Client{
 
 	/**
-	 * Components for login
+	 * Components for login screen
 	 */
-	private static JPanel helpp,panel, welcome, lmsg, lName, lPassword, enter, imgPanel;
+	private static JPanel helpPanel, mainPanel, welcome, lmsg, lName, lPassword, enter, imgPanel;
 	private static JFrame frame;
-	private static JButton login, help;
+	private static JButton login;
 	private static JTextField nameField;
 	private static JPasswordField passField;
 	private static MenuPanel mainMenu;
-	private static String logoName;
+	
 	/**
-	 * Main method
+	 * Filenames
+	 */
+	private static String logoFileName;
+	private static File helpFile;
+	private static ImageIcon helpimg;
+	
+	/**
+	 * Main method - Creates login panel, handles login information
+	 * 
 	 * @param args no arguments used
 	 * @throws UnsupportedLookAndFeelException 
 	 * @throws IllegalAccessException 
@@ -56,14 +60,12 @@ public class Client{
 	 */
 	public static void main(String[] args){
 		
-		// To get the same look and feel of the system the program is run on
-		//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		frame = new JFrame("Tailored Travels");
 		Color bgcolor = new Color(74,138,255); //Background color for login screen
 
-		
-		panel = new JPanel();
-		helpp = new JPanel();
+		//Create panels for login screen
+		mainPanel = new JPanel();
+		helpPanel = new JPanel();
 		welcome = new JPanel();
 		lmsg = new JPanel();
 		lName = new JPanel();
@@ -72,19 +74,21 @@ public class Client{
 		imgPanel = new JPanel();
 		login = new JButton("Login");
 
-		panel.setBackground(bgcolor);
+		//Set background of all panels in login screen to the same color
+		mainPanel.setBackground(bgcolor);
 		imgPanel.setBackground(bgcolor);
 		welcome.setBackground(bgcolor);
 		lmsg.setBackground(bgcolor);
 		lName.setBackground(bgcolor);
 		lPassword.setBackground(bgcolor);
 		enter.setBackground(bgcolor);
-
+		
+		//Add label to top of login screen
 		welcome.add(new JLabel("Welcome to Tailored Travels!"));
 		
 		//Add logo, then resize and realign
-		logoName = "Final Draft of Logo.jpg";
-		ImageIcon teamLogo = new ImageIcon(logoName);
+		logoFileName = "Final Draft of Logo.jpg";
+		ImageIcon teamLogo = new ImageIcon(logoFileName);
 		Image img = teamLogo.getImage();
 		int width = img.getWidth(null);
 		int height = img.getHeight(null);
@@ -92,32 +96,37 @@ public class Client{
 		int resizeH = height/3;
 		Image resizedImg = img.getScaledInstance(resizeW, resizeH, Image.SCALE_SMOOTH);
 		teamLogo = new ImageIcon(resizedImg);
-		
 		frame.setIconImage(img);
 		JLabel logo = new JLabel(teamLogo);
 		imgPanel.add(logo);
+		
+		//Add username field and label
 		lmsg.add(new JLabel ("Enter your username and password"));
 		lName.add(new JLabel("Username"));
 		nameField = new JTextField(25);
 		lName.add(nameField);
 		
+		//Add password field and label
 		lPassword.add(new JLabel("Password"));
 		passField = new JPasswordField(25);
 		String bullet = "\u2022";
-		passField.setEchoChar(bullet.charAt(0));
+		passField.setEchoChar(bullet.charAt(0)); //Blank out password field
 		lPassword.add(passField);
 		enter.add(login);
 		
-		ImageIcon helpimg = new ImageIcon("help.png");
+		//Create the help button
+		helpimg = new ImageIcon("help.png");
 		JButton help = new JButton(helpimg);
 		help.setToolTipText("Click here for help.");
 		help.setBorder(BorderFactory.createEmptyBorder());
+		
+		//When user interacts with help button, load and open the help pdf file
 		help.addActionListener(new ActionListener(){public void actionPerformed (ActionEvent e)
 		{
 			if (Desktop.isDesktopSupported()) {
 			    try {
-			        File myFile = new File("Tailored Travels Help.pdf");
-			        Desktop.getDesktop().open(myFile);
+			        helpFile = new File("Tailored Travels Help.pdf");
+			        Desktop.getDesktop().open(helpFile);
 			    } catch (IOException ex) {
 			        // no application registered for PDFs
 			    	JOptionPane.showMessageDialog(null, "A PDF viewer is needed to view the help"
@@ -125,44 +134,51 @@ public class Client{
 			    }
 			}
 		}});
-		helpp.add(help);
-		helpp.setBackground(bgcolor);
-		help.setBackground(bgcolor);
-		panel.add(helpp);
-		panel.add(welcome);
-		panel.add(imgPanel);
-		panel.add(lmsg);
-		panel.add(lName);
-		panel.add(lPassword);
-		panel.add(enter);
 		
-		panel.setPreferredSize(new Dimension(500,425));
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		//Add help button to the main login screen
+		helpPanel.add(help);
+		helpPanel.setBackground(bgcolor);
+		help.setBackground(bgcolor);
+		
+		//Add all panels to the mainPanel in proper format
+		mainPanel.add(helpPanel);
+		mainPanel.add(welcome);
+		mainPanel.add(imgPanel);
+		mainPanel.add(lmsg);
+		mainPanel.add(lName);
+		mainPanel.add(lPassword);
+		mainPanel.add(enter);
+		
+		//Set mainPanel and frame attributes
+		mainPanel.setPreferredSize(new Dimension(500,425));
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		frame.setContentPane(panel);
+		frame.setContentPane(mainPanel);
 		frame.pack();
 		
-// check login credentials here ------------------------------------------------------
+		//Check login credentials when user interacts with login button
 		login.addActionListener(new ActionListener()
 		{
 			
 			/**
 			 * Overwritten method for ActionListener class. Verifies the user is
 			 * authorized to use the system.
+			 * 
 			 * @param e An ActionEvent is created when the user clicks the login button
 			 */
 			public void actionPerformed(ActionEvent e)
 			{
 				
+				//Get username and password, immediately blank out both fields
 				String userName = nameField.getText();
 				char[] password = passField.getPassword();
 				
 				nameField.setText(null);
 				passField.setText(null);
 				
+				//Try to instantiate DatabaseManager
 				DatabaseManager dbm = null;
-				
 				try 
 				{
 					dbm = new DatabaseManager();
@@ -179,18 +195,21 @@ public class Client{
 					System.exit(0);
 				}
 				
-				String passwd = "";
-				
 				//convert password to string
+				String passwd = "";
 				for (int i = 0; i < password.length; i++)
 					passwd += password[i];
 					
 				try 
 				{
+					//Is the user valid and in the database
 					boolean validUser = dbm.login(userName, passwd);
+					
 					// Java documentation recommends clearing password array after use
 					for (int i = 0; i < password.length; i++)
 						password[i] = ' ';
+					
+					//Is the user an admin
 					boolean admin = dbm.isUserAdmin();
 					if (!validUser) 
 					{
@@ -220,20 +239,22 @@ public class Client{
 	}
 	
 	/**
-	 * Opens up the Google Earth application and displays menu options for the user
+	 * Opens up the main menu screen
+	 * 
 	 * @param dbm the dbm that contains user and location information
 	 * @param userName the user's username
 	 * @param admin whether or not a user is an administrator
-	 * @param locations the list of waypoints
 	 */
 	public static void showMainMenu(DatabaseManager dbm, String userName, boolean admin)
 	{
 
+		//Get rid of the login frame and create new menuPanel
 		frame.dispose();
 		mainMenu = new MenuPanel(dbm, userName, admin);
 		
+		//Create new frame for menuPanel and add components
 		JFrame frame2 = new JFrame("Tailored Travels");
-		frame2.setIconImage(new ImageIcon(logoName).getImage());
+		frame2.setIconImage(new ImageIcon(logoFileName).getImage());
 		mainMenu.addComponents();
 		frame2.setContentPane(mainMenu);
 		frame2.setVisible(true);
